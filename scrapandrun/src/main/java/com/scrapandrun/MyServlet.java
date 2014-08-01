dabf558250ea0c194d5d167ff313d8927fb29772
David McCloskey <dave.mccloskey@gmail.com>
2014-07-31 21:52:18 -0700

    initial commit

---
+++ scrapandrun/src/main/java/com/scrapandrun/MyServlet.java
@@ -0,0 +1,27 @@
+package com.scrapandrun;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MyServlet extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws IOException {
+        resp.setContentType("text/plain");
+        resp.getWriter().println("Please use the form to POST to this url");
+    }
+
+    @Override
+    public void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws IOException {
+        String name = req.getParameter("name");
+        resp.setContentType("text/plain");
+        if (name == null) {
+            resp.getWriter().println("Please enter a name");
+        }
+        resp.getWriter().println("Hello " + name);
+    }
+}
